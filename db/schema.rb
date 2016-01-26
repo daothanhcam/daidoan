162ed34d5071e29dbd3d540c348c4dd7aa5e70d1
daothanhcam <daothanhcam@gmail.com>
2016-01-26 19:25:48 +0700

    init models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,88 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160106091816) do
+ActiveRecord::Schema.define(version: 20160126121816) do
+
+  create_table "banners", force: :cascade do |t|
+    t.integer  "post_id",    limit: 4
+    t.date     "start_date"
+    t.date     "end_date"
+    t.boolean  "actived"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "banners", ["post_id"], name: "index_banners_on_post_id", using: :btree
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.integer  "post_id",    limit: 4
+    t.boolean  "hidden"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
+
+  create_table "converations", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "converations", ["user_id"], name: "index_converations_on_user_id", using: :btree
+
+  create_table "feedbacks", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.string   "title",      limit: 255
+    t.text     "content",    limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_index "feedbacks", ["user_id"], name: "index_feedbacks_on_user_id", using: :btree
+
+  create_table "followings", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.integer  "post_id",    limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "followings", ["post_id"], name: "index_followings_on_post_id", using: :btree
+  add_index "followings", ["user_id"], name: "index_followings_on_user_id", using: :btree
+
+  create_table "images", force: :cascade do |t|
+    t.integer  "post_id",    limit: 4
+    t.integer  "banner_id",  limit: 4
+    t.string   "url",        limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "images", ["banner_id"], name: "index_images_on_banner_id", using: :btree
+  add_index "images", ["post_id"], name: "index_images_on_post_id", using: :btree
+
+  create_table "messages", force: :cascade do |t|
+    t.integer  "converation_id", limit: 4
+    t.integer  "user_id",        limit: 4
+    t.text     "content",        limit: 65535
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+  end
+
+  add_index "messages", ["converation_id"], name: "index_messages_on_converation_id", using: :btree
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.string   "type",       limit: 255
+    t.text     "content",    limit: 65535
+    t.integer  "view",       limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -31,4 +112,15 @@ ActiveRecord::Schema.define(version: 20160106091816) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "banners", "posts"
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "converations", "users"
+  add_foreign_key "feedbacks", "users"
+  add_foreign_key "followings", "posts"
+  add_foreign_key "followings", "users"
+  add_foreign_key "images", "banners"
+  add_foreign_key "images", "posts"
+  add_foreign_key "messages", "converations"
+  add_foreign_key "messages", "users"
 end
